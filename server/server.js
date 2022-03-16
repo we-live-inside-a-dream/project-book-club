ddad4ff471b3977f53541cc89d6eb8b079e85d75
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-15 20:25:43 -0600

    updated the server with create event route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+
+const eventRoutes = require('./routes/eventRoutes')
+
+
+
 const app = express()
+const port = 5000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.use('/api/eventRoutes', eventRoutes)
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
-
-app.listen(3000)
