09e0fd9c5e42677e9f20f44677873c980104cd65
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-17 00:58:32 -0600

    changed the name of the collection

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-let dbUrl = 'mongodb+srv://Reza:l3t-m3-1n@cluster0.zbhoc.mongodb.net/events?retryWrites=true&w=majority'
+let dbUrl = 'mongodb+srv://Reza:l3t-m3-1n@cluster0.zbhoc.mongodb.net/bookClubDb?retryWrites=true&w=majority'
 mongoose.connect(dbUrl)
 
 module.exports = mongoose
