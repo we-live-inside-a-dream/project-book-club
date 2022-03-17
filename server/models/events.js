f2bf91b4a580e2e046e440534433a814c32b8a75
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-16 23:39:36 -0600

    comented out stuff to see if data can be sent

---
+++ server/models/events.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const Event = mongoose.model("events", {
+    title: {
+        type: "string",
+        //required: true
+    },
+    // userProfileId: {
+    //     type: "string",
+    //     required: true
+    // },
+    firstName: {
+        type: "string",
+        //required: true
+    },
+    lastName: {
+        type: "string",
+        //required: true
+    },
+    type: {
+        type: [String],
+        //requited: true
+    },
+    recurring: {
+        type: Boolean,
+    },
+    // startDate: {
+    //     type: String,
+    //     required: true
+    // },
+    // endDate: {
+    //     type: String,
+    //     required: true
+    // },
+    startTime: {
+        type: String,
+
+    },
+    endTime: {
+        type: String,
+    },
+    notes: {
+        type: String,
+    },
+    visibility: {
+        type: Boolean,
+    }
+})
+
+async function createEvent(eventData) {
+    let newEvent = new Event(eventData)
+    let createdEvent = await newEvent.save()
+    console.log("saving event info", createdEvent)
+    return createdEvent._id;
+}
+
+module.exports = {
+    createEvent
+}
