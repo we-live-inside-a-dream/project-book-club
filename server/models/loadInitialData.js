b7bced93d102fcaf10b01b6116ca78cb8d02a509
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-17 13:33:24 -0600

    changed password and database name

--- server/models/loadInitialData.js
+++ server/models/loadInitialData.js
@@ -2,22 +2,22 @@
 let studentsList = require("./studentsList.json");
 let studentsListModel = require("./student");
 
-const loadInitialData = async () => {
-  for (let student of studentsList) {
-    console.log("added student to list", student.name);
-    let createdId = await studentsListModel.createStudent(student);
-    console.log(
-      "student first name ",
-      student.name,
-      "created with id",
-      createdId
-    );
-  }
-};
-loadInitialData();
-// studentsList.forEach(async (student) => {
-//     console.log("Creating shift for", student.name);
+// const loadInitialData = async () => {
+//   for (let student of studentsList) {
+//     console.log("added student to list", student.name);
 //     let createdId = await studentsListModel.createStudent(student);
-//     console.log("... created with id", createdId);
-//   });
+//     console.log(
+//       "student first name ",
+//       student.name,
+//       "created with id",
+//       createdId
+//     );
+//   }
+// };
+// loadInitialData();
+studentsList.forEach(async (student) => {
+    console.log("Creating shift for", student.name);
+    let createdId = await studentsListModel.createStudent(student);
+    console.log("... created with id", createdId);
+  });
   
