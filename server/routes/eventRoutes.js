245624f7653bf361049077906851e24d54774144
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-17 10:16:47 -0600

    changed the routes to match the json data/success with REST

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = express.Router()
-const eventModel = require('../models/events')
+//const eventModel = require('../models/events')
+const studentsList = require('../models/studentsList.json')
 
-router.post('/createEvent', async (req, res) => {
-    let newEvent = req.body
-    let createdEventId = await eventModel.createEvent(newEvent)
-    res.send(createdEventId)
-    console.log("from API event route eventId", createdEventId)
+// router.get('/createEvent', async (req, res) => {
+//     let newEvent = req.body
+//     let createdEventId = await eventModel.createEvent(newEvent)
+//     res.send(createdEventId)
+//     console.log("from API event route eventId", createdEventId)
+// })
+router.get('/createEvent', async (req, res) => {
+    res.send(studentsList)
+
 })
 
 
