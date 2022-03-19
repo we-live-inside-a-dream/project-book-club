1e624127107161442ec85c6db0bc31d95542b3ef
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-19 00:10:23 -0600

    fixed route

--- server/routes/membersRoutes.js
+++ server/routes/membersRoutes.js
@@ -3,24 +3,24 @@ const router = express.Router()
 
 const membersListModel = require('../models/members')
 
-router.get('/members', async(req, res) => {
+router.get('/members/list', async(req, res) => {
     let membersList = await membersListModel.listMembers()
     res.send(membersList)
 })
 
-router.post('members', async (req, res) => {
+router.post('/members/create', async (req, res) => {
     let newMember = req.body
     let createdId = await membersListModel.createMember(newMember)
     res.send(createdId)
 })
 
-router.get('members/:id', async(req, res) => {
+router.get('/members/:id', async(req, res) => {
     let id = req.params.id
     let member = await membersListModel.findById(id)
     res.send(member)
 })
 
-router.get('members/:id', async (req, res) => {
+router.get('/members/update/:id', async (req, res) => {
     let id = req.params.id
     let updatedMember = req.body
     console.log("updating members", id, "with", updatedMember)
