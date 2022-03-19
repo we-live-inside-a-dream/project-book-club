34dbff6ca2ba48dfeec527b6d8d3eb18f1726e98
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-19 11:39:50 -0600

    added the json body middleware

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const eventRoutes = require('./routes/eventRoutes')
 const membersRoutes = require('./routes/membersRoutes')
 
 
-
 const app = express()
 const port = 5001
+
+app.use(express.json())
 
 app.use('/api/events', eventRoutes)
 app.use('/api/members', membersRoutes)
