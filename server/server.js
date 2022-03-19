99f48b959659dbecff6d1cf6832afcd19da8f503
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-19 00:13:52 -0600

    fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const eventRoutes = require('./routes/eventRoutes')
-const membersRoutes = require('./routes/memberRoutes')
+const membersRoutes = require('./routes/membersRoutes')
 
 
 
