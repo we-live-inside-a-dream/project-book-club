f97a73750978c9e91411b4b502cb4a8dd5b49ef3
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-19 01:08:18 -0600

    changed create function

--- server/models/members.js
+++ server/models/members.js
@@ -2,13 +2,21 @@
 const mongoose = require('./mongooseDb');
 
 const Members = mongoose.model('Members', {
-    "firstName": String,
-    "lastName": String
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String
+    }
+    // "firstName": String,
+    // "lastName": String
 })
 
 
-async function createMember(MemberData) {
-    let createdMember = await Members.create(MemberData)
+async function createMember(membersData) {
+    let newMember = new Members(membersData)
+    console.log("this is a new member", newMember)
+    let createdMember = await newMember.save()
     console.log("saving members info", createdMember)
     return createdMember.id;
 }
@@ -21,8 +29,12 @@ async function findById(id) {
     return Members.findById(id)
 }
 
-async function update(id, newMember) {
-    return Members.findByIdAndUpdate(id, newMember)
+async function update(id, updatedMember) {
+    console.log("From the model/update", id, updatedMember)
+    await Members.findByIdAndUpdate(id, updatedMember, {
+        returnDocument: "after"
+    })
+    // return Members.findByIdAndUpdate(id, newMember)
 }
 
 module.exports = {
