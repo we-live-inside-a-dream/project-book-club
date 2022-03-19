70dd28077a6c69bb854c4272844ffe5f58b63ce1
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-19 09:54:45 -0600

    added update/delete routes

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router()
-//const eventModel = require('../models/events')
-const studentsList = require('../models/membersList.json')
+const eventModel = require('../models/events')
+// const studentsList = require('../models/membersList.json')
 
-// router.get('/createEvent', async (req, res) => {
-//     let newEvent = req.body
-//     let createdEventId = await eventModel.createEvent(newEvent)
-//     res.send(createdEventId)
-//     console.log("from API event route eventId", createdEventId)
-// })
-router.get('/createEvent', async (req, res) => {
-    res.send(membersList)
 
+router.post('/createEvent', async (req, res) => {
+    let newEvent = req.body
+    let createdEventId = await eventModel.createEvent(newEvent)
+    res.send(createdEventId)
+    console.log("from API event route eventId", createdEventId)
 })
 
+
+router.post('/updateEvent:id', async (req, res) => {
+    let id = req.params.id
+    let updatedEvent = req.body
+    let newUpdatedEvent = await eventModel.updateEvent(id, updatedEvent)
+    res.json(newUpdatedEvent)
+})
+
+
+//this route doesn't delete
+router.delete('/deleteEvent:id', async (req, res) => {
+    let id = req.params.id
+    let deleteEvent = await eventModel.deleteEvent(id)
+    res.send(deleteEvent)
+})
 
 module.exports = router
