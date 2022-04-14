3e97612dd695cf537aadac60a3590eafc756b6e0
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-14 10:11:48 -0600

    brought in router and home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {BasicComponent} from './components/NewComponent';
+import Home from './pages/Home';
+import { Routes, Route, Link } from "react-router-dom";
+
+
+
 function App() {
   return (
     <div>
-      <BasicComponent name='The work' color='green'/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      
     </div>
   );
 }
