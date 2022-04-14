3fdc6da24526c10c92fcfabcefaa324d3a2e1814
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-14 10:13:26 -0600

    created navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <>
+      <div class='navBar'>
+        <NavbarItem title={'Home'} address={'/'}/>
+        <NavbarItem title={'Members'} address={'/members'}/>
+        <NavbarItem title={'Schedule'} address={'/schedule'}/>
+        <NavbarItem title={'Resources'} address={'/resources'}/>
+      </div>
+    </>
+  );
+}
+
+export function NavbarItem(props) {
+    const {title, address} = props;
+
+    return(
+        
+        <div class='navbar-item'>
+            <a href={address} class='navbar-title'>{title}</a>
+        </div>
+        
+    )
+}
+
+export default Navbar
