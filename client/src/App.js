9d447b45691b850d78c78ea28a9f83d61049c6ea
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-15 16:35:41 -0600

    imported signup component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Resources from './pages/Resources';
 import Schedule from './pages/Schedule';
 import Members from './pages/Members';
+import SignupForm from './components/SignupForm';
 
 
 
@@ -17,6 +18,8 @@ function App() {
         <Route path="/resources" element={<Resources />}/>
         <Route path="/members" element={<Members />}/>
         <Route path="/schedule" element={<Schedule />}/>
+        <Route path="/signupForm" element={<SignupForm />}/>
+
       </Routes>
       
     </div>
