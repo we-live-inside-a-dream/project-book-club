83388850a565587cb5ca8b0dcdb5df1509ad9346
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-14 22:35:53 -0600

    added component paths to router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {BasicComponent} from './components/NewComponent';
 import Home from './pages/Home';
 import { Routes, Route, Link } from "react-router-dom";
+import Resources from './pages/Resources';
+import Schedule from './pages/Schedule';
+import Members from './pages/Members';
 
 
 
@@ -11,6 +14,9 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/resources" element={<Resources />}/>
+        <Route path="/members" element={<Members />}/>
+        <Route path="/schedule" element={<Schedule />}/>
       </Routes>
       
     </div>
