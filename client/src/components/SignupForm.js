f2bf2a7f502b764fd41d34f24013e6e0ef3dc839
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-15 20:51:00 -0600

    refactored code moved initialValues/onSubmit/validate to their own functions and added validation message to each field

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,40 +1,49 @@
 import React from "react";
 import { useFormik } from "formik";
 
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+};
+
+const onSubmit = (values) => {
+  //alert(JSON.stringify(values, null, 2));
+  console.log("Form data", values);
+};
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = "Required";
+  }
+  if (!values.lastName) {
+    errors.lastName = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)) {
+    errors.password =
+      "Password must be at least 8 characters and contain at least one number and one letter";
+  }
+  return errors;
+};
+
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
   const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-    validate: values => {
-        const errors = {};
-        if(!values.firstName){
-            errors.firstName = 'Required'
-        }
-        if(!values.lastName){
-            errors.lastName = 'Required'
-        }
-        if(!values.email){
-            errors.email = 'Required'
-        }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
-            errors.email = 'Invalid email address'
-        }
-        if(!values.password){
-            errors.password = 'Required'
-        }else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)){
-            errors.password = 'Password must be at least 8 characters and contain at least one number and one letter'
-        }
-        return errors;
-    }
+    initialValues,
+    onSubmit,
+    validate,
   });
+  console.log('form errors',formik.errors)
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
@@ -45,6 +54,8 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+      {formik.errors.firstName ? <div>{formik.errors.firstName}</div>: null}
+
       <label htmlFor="firstName">Last Name</label>
       <input
         id="lastName"
@@ -53,6 +64,8 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+      {formik.errors.lastName ? <div>{formik.errors.lastName}</div>: null}
+
       <label htmlFor="email">Email Address</label>
       <input
         id="email"
@@ -61,7 +74,8 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
-      
+      {formik.errors.email ? <div>{formik.errors.email}</div>: null}
+
       <label htmlFor="email">Password</label>
       <input
         id="password"
@@ -70,7 +84,7 @@ const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.password}
       />
-      
+      {formik.errors.password ? <div>{formik.errors.password}</div>: null}
 
       <button type="submit">Submit</button>
     </form>
