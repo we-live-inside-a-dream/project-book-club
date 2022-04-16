61a2d8cd8d02b70aa040acad2b24e56811291ea2
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-04-15 20:27:50 -0600

    included validation to the signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -14,6 +14,26 @@ const SignupForm = () => {
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
+    validate: values => {
+        const errors = {};
+        if(!values.firstName){
+            errors.firstName = 'Required'
+        }
+        if(!values.lastName){
+            errors.lastName = 'Required'
+        }
+        if(!values.email){
+            errors.email = 'Required'
+        }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)){
+            errors.email = 'Invalid email address'
+        }
+        if(!values.password){
+            errors.password = 'Required'
+        }else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i.test(values.password)){
+            errors.password = 'Password must be at least 8 characters and contain at least one number and one letter'
+        }
+        return errors;
+    }
   });
   return (
     <form onSubmit={formik.handleSubmit}>
