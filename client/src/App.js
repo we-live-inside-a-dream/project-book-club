fa03a503420ad50b85e5147c57698559087b3d9f
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-04-24 22:32:52 -0600

    created about us component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,23 @@
-
-import './App.css';
-import {BasicComponent} from './components/NewComponent';
-import Home from './pages/Home';
+import "./App.css";
+import { BasicComponent } from "./components/NewComponent";
+import Home from "./pages/Home";
 import { Routes, Route, Link } from "react-router-dom";
-import Resources from './pages/Resources';
-import Schedule from './pages/Schedule';
-import Members from './pages/Members';
-import SignupForm from './components/SignupForm';
-
-
+import Resources from "./pages/Resources";
+import Schedule from "./pages/Schedule";
+import Members from "./pages/Members";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
-
+    <div>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/resources" element={<Resources />}/>
-        <Route path="/members" element={<Members />}/>
-        <Route path="/schedule" element={<Schedule />}/>
-        <Route path="/signupForm" element={<SignupForm />}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/members" element={<Members />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/signupForm" element={<SignupForm />} />
       </Routes>
-
+    </div>
   );
 }
 
---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,11 @@
+
+function AboutUs(){
+    return (
+        <div>
+            <h1>About Our Book Club</h1>
+            <p>This book club was found by Babak Lotfikish in 2015</p>
+        </div>
+    )
+}
+
+export default AboutUs;
