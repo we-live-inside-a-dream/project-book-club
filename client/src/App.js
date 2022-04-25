271ffb01c51f018f5716ba641dc003628d1cf04f
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-04-25 08:57:48 -0600

    brought in the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,24 @@ import { Routes, Route, Link } from "react-router-dom";
 import Resources from "./pages/Resources";
 import Schedule from "./pages/Schedule";
 import Members from "./pages/Members";
-import SignupForm from "./components/SignupForm";
+import SignupForm from "./pages/SignupForm";
+import Navbar from "./components/Navbar";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <div>
+      <div class="header">
+        <Navbar />
+        <h1 class="welcome">The Work Book Club</h1>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/members" element={<Members />} />
         <Route path="/schedule" element={<Schedule />} />
         <Route path="/signupForm" element={<SignupForm />} />
+        <Route path="/signIn" element={<SignIn />} />
       </Routes>
     </div>
   );
