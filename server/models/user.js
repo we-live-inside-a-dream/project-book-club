5175f8080922705cbcb9dd263016939c012ebbc8
reza <rezanaeimabadi7@gmail.com>
2023-01-15 14:18:26 -0700

    created user mongoose schema

---
+++ server/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  username: String,
+  password: String,
+});
+
+// Export Model
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", User);
