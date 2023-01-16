ebb08d5fa5fdab1d7dfe059f44f9e5567ae3c835
reza <rezanaeimabadi7@gmail.com>
2023-01-16 09:26:26 -0700

    moved the crud functions to routes/repo

--- server/models/members.js
+++ server/models/members.js
@@ -22,36 +22,3 @@ const Members = mongoose.model('Members', {
     }
     
 })
-
-
-async function createMember(membersData) {
-    let newMember = new Members(membersData)
-    console.log("this is a new member", newMember)
-    let createdMember = await newMember.save()
-    console.log("saving members info", createdMember)
-    return createdMember.id;
-}
-
-async function listMembers() {
-    return Members.find({})
-}
-
-async function findById(id) {
-    return Members.findById(id)
-}
-
-async function updateMember(id, updatedMember) {
-    console.log("From the model/update", id, updatedMember)
-    await Members.findByIdAndUpdate(id, updatedMember, {
-        returnDocument: "after"
-    })
-    // return Members.findByIdAndUpdate(id, newMember)
-}
-
-module.exports = {
-    createMember,
-    listMembers,
-    findById,
-    updateMember
-}
-
