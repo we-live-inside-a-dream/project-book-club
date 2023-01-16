398078ab1710b88563226b1efe0d384516a30d36
reza <rezanaeimabadi7@gmail.com>
2023-01-16 09:52:57 -0700

    modified session middleware

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ app.use('/api/events', eventRoutes)
 app.use('/api/members', membersRoutes)
 
 app.use(session({
-  secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+  secret: 'secretcode',
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
   cookie: { maxAge: 60 * 60 * 1000} // 1 hour
 }));
 
