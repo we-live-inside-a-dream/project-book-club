33c2b064a38a72d591eabf17f4c4fe8e971ef9d9
Reza Naeim-Abadi <reza@hometeamlive.com>
2023-01-17 20:30:00 -0700

    Formatted file

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,22 +1,21 @@
-
 import { useFormik } from "formik";
-import './signupForm.css'
+import "./signupForm.css";
 
 const initialValues = {
   email: "",
   password: "",
 };
 
-export const baseUrl = "http://localhost:5001"
+export const baseUrl = "http://localhost:5001";
 const onSubmit = async (values) => {
-  await fetch(`${baseUrl}/login`, {
-    method: 'POST',
+  await fetch(`${baseUrl}/api/auth/login`, {
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Origin': baseUrl
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Origin: baseUrl,
     },
-    body: JSON.stringify(values)
+    body: JSON.stringify(values),
   });
 };
 
@@ -38,15 +37,17 @@ const SignIn = () => {
     onSubmit,
     validate,
   });
-  console.log('form errors', formik.errors)
+  console.log("form errors", formik.errors);
   return (
-    <div class='background'>
-      <div class='secondBackground'>
-        <form class='form' onSubmit={formik.handleSubmit}>
-
-          <div class='container'>
-            <label class='label' htmlFor="email">Email Address</label>
-            <input class='input'
+    <div class="background">
+      <div class="secondBackground">
+        <form class="form" onSubmit={formik.handleSubmit}>
+          <div class="container">
+            <label class="label" htmlFor="email">
+              Email Address
+            </label>
+            <input
+              class="input"
               id="email"
               name="email"
               type="email"
@@ -56,26 +57,30 @@ const SignIn = () => {
             {formik.errors.email ? <div>{formik.errors.email}</div> : null}
           </div>
 
-          <div class='container'>
-            <label class='label' htmlFor="email">Password</label>
-            <input class='input'
+          <div class="container">
+            <label class="label" htmlFor="email">
+              Password
+            </label>
+            <input
+              class="input"
               id="password"
               name="password"
               type="password"
               onChange={formik.handleChange}
               value={formik.values.password}
             />
-            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
+            {formik.errors.password ? (
+              <div>{formik.errors.password}</div>
+            ) : null}
           </div>
 
           <button type="submit">Sign In</button>
         </form>
-        <p>Don't have an account yet?
-            <br />
-        <a href="/signupForm"> Register </a> here!
+        <p>
+          Don't have an account yet?
+          <br />
+          <a href="/signupForm"> Register </a> here!
         </p>
-
-
       </div>
     </div>
   );
