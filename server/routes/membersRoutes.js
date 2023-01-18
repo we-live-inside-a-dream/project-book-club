5b455ac2ab037d77c2addaa9bd4343cf7ec9acd0
Reza Naeim-Abadi <reza@hometeamlive.com>
2023-01-17 20:32:30 -0700

    commented out the code for now

--- server/routes/membersRoutes.js
+++ server/routes/membersRoutes.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const router = express.Router()
+// const express = require('express');
+// const router = express.Router()
 
-const membersListModel = require('../models/members')
+// const membersListModel = require('../models/members')
 
-router.post('/create', async (req, res) => {
-    let newMember = req.body
-    let createdId = await membersListModel.createMember(newMember)
-    res.send(createdId)
-    console.log("from API this is a new member", createdId)
-})
+// router.post('/create', async (req, res) => {
+//     let newMember = req.body
+//     let createdId = await membersListModel.createMember(newMember)
+//     res.send(createdId)
+//     console.log("from API this is a new member", createdId)
+// })
 
-router.get('/list', async(req, res) => {
-    let membersList = await membersListModel.listMembers()
-    res.send(membersList)
-})
+// router.get('/list', async(req, res) => {
+//     let membersList = await membersListModel.listMembers()
+//     res.send(membersList)
+// })
 
 
-router.get('/:id', async(req, res) => {
-    let id = req.params.id
-    let member = await membersListModel.findById(id)
-    res.send(member)
-})
+// router.get('/:id', async(req, res) => {
+//     let id = req.params.id
+//     let member = await membersListModel.findById(id)
+//     res.send(member)
+// })
 
-//not working :(
-router.post('/update/:id', async (req, res) => {
-    let id = req.params.id
-    let updatedMember = req.body
-    console.log("updating members", id, "with", updatedMember)
-    let newUpdatedMember = await membersListModel.updateMember(id, updatedMember)
-    console.log("this is newUpdatedMember from routes", newUpdatedMember)
-    res.send(newUpdatedMember)
-})
+// //not working :(
+// router.post('/update/:id', async (req, res) => {
+//     let id = req.params.id
+//     let updatedMember = req.body
+//     console.log("updating members", id, "with", updatedMember)
+//     let newUpdatedMember = await membersListModel.updateMember(id, updatedMember)
+//     console.log("this is newUpdatedMember from routes", newUpdatedMember)
+//     res.send(newUpdatedMember)
+// })
 
-module.exports = router
+// module.exports = router
