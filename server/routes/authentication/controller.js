aeadaadfe584df9800c38390a015adc10e0d2e8d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-19 16:47:06 -0700

    exported controller

--- server/routes/authentication/controller.js
+++ server/routes/authentication/controller.js
@@ -1,4 +1,3 @@
-
 const passport = require("passport");
 const memberRepo = require("./repository");
 const LocalStrategy = require("passport-local").Strategy;
@@ -6,31 +5,35 @@ const LocalStrategy = require("passport-local").Strategy;
 // Passport Local Strategy
 passport.use(
   new LocalStrategy(function (username, password, done) {
-    console.log("passport is trying to verify a member",username )
-    memberRepo.findUserByUsername(username)
+    console.log("passport is trying to verify a member", username);
+    memberRepo
+      .findUserByUsername(username)
       .then((member) => {
         if (!member || member.password !== password) {
           done(null, false, { message: "Incorrect username or password." });
         }
         done(null, member);
       })
-      .catch(done)
+      .catch(done);
   })
 );
 
-passport.serializeUser(function(member, done) {
-  console.log("passport wants to store this member in a cookie", member)
+passport.serializeUser(function (member, done) {
+  console.log("passport wants to store this member in a cookie", member);
   done(null, member.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  console.log('passport is trying to recover the member from the cookie', id)
-  memberRepo.findById(id)
+passport.deserializeUser(function (id, done) {
+  console.log("passport is trying to recover the member from the cookie", id);
+  memberRepo
+    .findById(id)
     .then((member) => {
       if (!member) {
-        done(new Error("Member not found or deleted"))
+        done(new Error("Member not found or deleted"));
       }
       done(null, member);
     })
-    .catch(done) 
+    .catch(done);
 });
+
+module.exports = passport;
