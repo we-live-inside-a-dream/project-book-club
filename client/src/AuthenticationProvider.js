1b3e309f39f86d9a9a3de9d82c1e8b6623cdd83d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-21 12:36:21 -0700

    completed authenticationProvider

--- client/src/AuthenticationProvider.js
+++ client/src/AuthenticationProvider.js
@@ -2,6 +2,23 @@ import React from 'react'
 import AuthenticationContext from './AuthenticationContext'
 
 const AuthenticationProvider = ({children}) => {
+    const [username, setUsername] = useState('')
+
+
+    const logIn = (logInUsername) => {
+        setUsername(logInUsername)
+    }
+
+    const logOut = () => {
+        setUsername('')
+    }
+
+    const authContext = {
+        username,
+        logIn,
+        logOut
+    }
+
     return (
         <AuthenticationContext.Provider value={authContext}>
             {children}
