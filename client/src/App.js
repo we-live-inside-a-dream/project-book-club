2c7f74b38d46453005c3bd6196794c5f08cd983b
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-21 18:35:25 -0700

    added authprovider

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
 import "./App.css";
-import { BasicComponent } from "./components/NewComponent";
 import Home from "./pages/Home";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Resources from "./pages/Resources";
 import Schedule from "./pages/Schedule";
 import Members from "./pages/Members";
 import SignupForm from "./pages/SignupForm";
 import Navbar from "./components/Navbar";
 import SignIn from "./pages/SignIn";
+import AuthenticationProvider from "./AuthenticationProvider";
 
 function App() {
   return (
-    <div>
-      <div class="header">
-        <Navbar />
-        <h1 class="welcome">The Work Book Club</h1>
+    <AuthenticationProvider>
+      <div>
+        <div class="header">
+          <Navbar />
+          <h1 class="welcome">The Work Book Club</h1>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/signupForm" element={<SignupForm />} />
+          <Route path="/signIn" element={<SignIn />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/schedule" element={<Schedule />} />
-        <Route path="/signupForm" element={<SignupForm />} />
-        <Route path="/signIn" element={<SignIn />} />
-      </Routes>
-    </div>
+    </AuthenticationProvider>
   );
 }
 
