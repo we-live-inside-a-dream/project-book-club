b94b4f104945353849c44c815f548071fe6967e4
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-22 18:59:55 -0700

    show users name once they login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,36 @@
-import './Navbar.css'
+import "./Navbar.css";
+import { useState } from "react";
+import { useContext } from "react";
+import AuthenticationContext from "../AuthenticationContext";
 
 function Navbar() {
+  const authContext = useContext(AuthenticationContext);
   return (
     <>
-        <div class='signupButton'>
-        <NavbarItem title={'Sign In / Register'} address={'/signIn'}/>
-        </div>
-      <div class='navBar'>
-        <NavbarItem title={'Home'} address={'/'}/>
-        <NavbarItem title={'Members'} address={'/members'}/>
-        <NavbarItem title={'Schedule'} address={'/schedule'}/>
-        <NavbarItem title={'Resources'} address={'/resources'}/>
+      <div class="signupButton">
+        <NavbarItem title={"Sign In / Register"} address={"/signIn"} />
+      </div>
+      <div class="navBar">
+        {authContext.user && <p>{authContext.user.firstName}</p>}
+        <NavbarItem title={"Home"} address={"/"} />
+        <NavbarItem title={"Members"} address={"/members"} />
+        <NavbarItem title={"Schedule"} address={"/schedule"} />
+        <NavbarItem title={"Resources"} address={"/resources"} />
       </div>
     </>
   );
 }
 
 export function NavbarItem(props) {
-    const {title, address} = props;
+  const { title, address } = props;
 
-    return(
-        
-        <div class='navbar-item'>
-            <a href={address} class='navbar-title'>{title}</a>
-        </div>
-        
-    )
+  return (
+    <div class="navbar-item">
+      <a href={address} class="navbar-title">
+        {title}
+      </a>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
