6087782545340d60016bd9a6788d103bef109ea8
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-22 18:58:41 -0700

    included login fetch

--- client/src/AuthenticationProvider.js
+++ client/src/AuthenticationProvider.js
@@ -1,29 +1,48 @@
-import React from 'react'
-import AuthenticationContext from './AuthenticationContext'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import AuthenticationContext from "./AuthenticationContext";
 
-const AuthenticationProvider = ({children}) => {
-    const [username, setUsername] = useState('')
+const AuthenticationProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
+  //   useEffect(() => {
 
-    const logIn = (logInUsername) => {
-        setUsername(logInUsername)
+  //   }, []);
+
+  const logIn = async (values) => {
+    const response = await fetch(`/api/member/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    if (response.ok) {
+        const userData = await response.json();
+        console.log('user data', userData);
+      setUser(userData);
+      return true;
+    } else {
+      return false;
     }
+  };
 
-    const logOut = () => {
-        setUsername('')
-    }
+  const logOut = () => {
+    fetch(`/api/member/login`);
+    setUser(null);
+  };
 
-    const authContext = {
-        username,
-        logIn,
-        logOut
-    }
+  const authContext = {
+    user,
+    logIn,
+    logOut,
+  };
 
-    return (
-        <AuthenticationContext.Provider value={authContext}>
-            {children}
-        </AuthenticationContext.Provider>
-    )
-}
+  return (
+    <AuthenticationContext.Provider value={authContext}>
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
 
-export default AuthenticationProvider
+export default AuthenticationProvider;
