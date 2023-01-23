3830507393678593e7374259c952bcf1d6dc837d
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-22 19:02:26 -0700

    exported controller

--- server/routes/events/controller.js
+++ server/routes/events/controller.js
@@ -1,11 +1,11 @@
-
 const eventRepo = require("./repository");
 
-
 const mustBeLoggedIn = (req, res, next) => {
-    if (req.user) {
-        next();
-    } else {
-        res.sendStatus(401);
-    }
-    }
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = mustBeLoggedIn;
