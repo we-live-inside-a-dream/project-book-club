26c2ec856ecff5361db5d2f9feeb4fa1609086c0
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-25 08:13:17 -0700

    updated indentation

--- server/models/user.js
+++ server/models/user.js
@@ -1,26 +1,24 @@
+const mongoose = require("./mongooseDb");
 
-const mongoose = require('./mongooseDb');
-
-const User = mongoose.model('User', {
-    firstName: {
-        type: String,
-    },
-    lastName: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    subscriptionDate: {
-        type: Date,
-    },
-    userName: {
-        type: String
-    }
-    
-})
+const User = mongoose.model("User", {
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  subscriptionDate: {
+    type: Date,
+  },
+  userName: {
+    type: String,
+  },
+});
 
 module.exports = User;
