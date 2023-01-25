e3a93a40b9a4ae5c35c207197a03441d3a89481e
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-01-25 09:01:54 -0700

    installed bcrypt and hashed users passwords

--- server/routes/authentication/routes.js
+++ server/routes/authentication/routes.js
@@ -6,13 +6,36 @@ const router = express.Router();
 const userRepo = require("./repository");
 const controllers = require("./controller");
 const User = require("../../models/user");
+const bcrypt = require("bcrypt");
 
+const saltRounds = 10;
 // Post Route: /login
-router.post("/login", passport.authenticate("local"), function (req, res) {
-  console.log("user logged in susscess");
-  console.log("from API login route", req.user);
-  res.send(req.user);
-});
+router.post(
+  "/login",
+  passport.authenticate("local"),
+  async function (req, res) {
+    try {
+      const user = await userRepo.findUserByUsername(req.body.username);
+      console.log("This is the user", user);
+      if (user) {
+        const cmp = await bcrypt.compare(req.body.password, user.password);
+        if (cmp) {
+          res.send(user);
+        } else {
+          res.status(401).send("Invalid username or password");
+        }
+      } else {
+        res.status(401).send("Invalid username or password");
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+    // console.log("user logged in susscess");
+    // console.log("from API login route", req.user);
+    // res.send(req.user);
+  }
+);
 
 router.get("/loggedInUser", (req, res) => {
   res.send(req.user);
@@ -26,11 +49,22 @@ router.get("/logout", (req, res) => {
 });
 
 router.post("/create", async (req, res) => {
-  let newUser = req.body;
-  let createdId = await userRepo.createUser(newUser);
-  res.send(createdId);
-  console.log("from API this is a new user", createdId);
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password, saltRounds);
+    const insertResult = await userRepo.createUser({
+      username: req.body.username,
+      password: hashedPwd,
+    });
+    res.send(insertResult);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
+// let newUser = req.body;
+// let createdId = await userRepo.createUser(newUser);
+// res.send(createdId);
+// console.log("from API this is a new user", createdId);
 
 router.get("/list", async (req, res) => {
   let userRepo = await userRepo.listUser();
