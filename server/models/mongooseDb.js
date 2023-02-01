4f143c187d879517c686d407ea0ab7d5626a2d52
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-02-01 07:04:55 -0700

    formatted code

--- server/models/mongooseDb.js
+++ server/models/mongooseDb.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-let dbUrl = 'mongodb+srv://Reza:Modernwarfare2@cluster0.zbhoc.mongodb.net/BookClub?retryWrites=true&w=majority'
-mongoose.connect(dbUrl)
+let dbUrl =
+  "mongodb+srv://Reza:Modernwarfare2@cluster0.zbhoc.mongodb.net/BookClub?retryWrites=true&w=majority";
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-module.exports = mongoose
+module.exports = mongoose;
