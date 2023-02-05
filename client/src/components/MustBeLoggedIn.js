84f695a35ad9c4d5213a014f2e3196acf70dac1a
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2023-02-05 14:34:32 -0700

    fixed useSelector not sending user info

--- client/src/components/MustBeLoggedIn.js
+++ client/src/components/MustBeLoggedIn.js
@@ -1,8 +1,9 @@
 import { useSelector } from "react-redux";
-
+import { selectUser } from "../redux/slice/userSlice";
 
 function MustBeLoggedIn({ children }) {
-  const userData = useSelector((state) => state.user);
+  const userData = useSelector(selectUser);
+  console.log("userData-mustBeLoggedIn", userData);
 
   return userData ? children : null;
 }
